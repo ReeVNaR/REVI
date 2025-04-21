be86bbb9cb93847b3df93fda77eb34120543cad2
ReeVNaR <ranveerghorpade16@gmail.com>
2025-04-21 21:11:59 +0530

    Revi_Ai

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.PROD ? 'https://reviai.vercel.app' : '/'}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
